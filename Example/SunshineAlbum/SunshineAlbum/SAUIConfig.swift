405b3a83ce03cb5086676f3fa7fa584878213870
GarenChen <915574843@qq.com>
2017-09-08 17:49:34 +0800

    add ui config

--- Example/SunshineAlbum/SunshineAlbum/SAUIConfig.swift
+++ Example/SunshineAlbum/SunshineAlbum/SAUIConfig.swift
@@ -19,4 +19,14 @@ class SAUIConfig {
 	
 	var tintColor = UIColor(redValue: 26, greenValue: 172, blueValue: 25, alpha: 0.95)
 	
+	var buttonDisableColor = UIColor(redValue: 22, greenValue: 82, blueValue: 23, alpha: 0.95)
+	
+	var disableColor = UIColor(redValue: 22, greenValue: 82, blueValue: 23, alpha: 0.95)
+	
+	var normalTextColor = UIColor.white
+	
+	var lightGrayTextColor = UIColor(redValue: 165, greenValue: 165, blueValue: 165, alpha: 0.95)
+	
+	var disableTextColor = UIColor(redValue: 103, greenValue: 107, blueValue: 112, alpha: 0.95)
+	
 }
