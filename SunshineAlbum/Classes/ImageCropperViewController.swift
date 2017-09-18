d88ec82023313406fb7523b2e941587f53b298d3
GarenChen <915574843@qq.com>
2017-09-18 17:56:59 +0800

    add single selection

--- SunshineAlbum/Classes/ImageCropperViewController.swift
+++ SunshineAlbum/Classes/ImageCropperViewController.swift
@@ -12,9 +12,56 @@ class ImageCropperViewController: UIViewController {
 	
 	var assetModel: AssetModel?
 	
-	convenience init(assetModel: AssetModel) {
+	var imageCropFrame: CGRect = .zero
+	
+	var originalImage: UIImage? {
+		didSet {
+			guard let image = originalImage else { return }
+			let oldWidth = imageCropFrame.size.width
+			let oldHeight = image.size.height * (oldWidth / image.size.height)
+			let oldX = imageCropFrame.origin.x + (imageCropFrame.width - oldWidth) / 2
+			let oldY = imageCropFrame.origin.y + (imageCropFrame.size.height - oldHeight) / 2
+			oldFrame = CGRect(x: oldX, y: oldY, width: oldWidth, height: oldHeight)
+		}
+	}
+	
+	var oldFrame: CGRect = .zero
+	
+	var largeFrame: CGRect = .zero
+	
+	var limitRatio: CGFloat = 0
+	
+	var latestFrame: CGRect = .zero
+	
+	lazy var imageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.frame = UIScreen.main.bounds
+		imageView.isUserInteractionEnabled = true
+		imageView.isMultipleTouchEnabled = true
+		return imageView
+	}()
+	
+	lazy var overLayView: UIView = {
+		let overLayView = UIView()
+		
+		return overLayView
+	}()
+	
+	lazy var ratioView: UIView = {
+		let ratioView = UIView()
+		
+		return ratioView
+	}()
+	
+	convenience init(assetModel: AssetModel, imageCropFrame: CGRect = SASelectionManager.shared.imageCropFrame) {
 		self.init()
 		self.assetModel = assetModel
+		self.imageCropFrame = imageCropFrame
+		AssetsManager.shared.fetchPreviewImage(asset: assetModel.asset) { [weak self] (image) in
+			guard let `self` = self else { return }
+			self.originalImage = image
+			self.imageView.image = image
+		}
 	}
 	
 	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -28,9 +75,17 @@ class ImageCropperViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		setupViews()
         // Do any additional setup after loading the view.
     }
+	
+	private func setupViews() {
+		view.backgroundColor = .black
+		
+		view.addSubview(imageView)
+		
+		
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
